823755cee373d34b4e087b0e5a43cf9c3a86a72b
@FranCarn <carnevalefranco99@gmail.com>
2023-04-18 15:08:20 -0300

    feat(Loader): loader new styles

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { PuffLoader } from "react-spinners";
+import styles from "./Loader.module.scss";
+
 export const Loader = () => {
   return (
-    <div>
+    <div className={styles.loader}>
       <PuffLoader color="#36d7b7" />
+      <span className={styles.loaderText}>Loading...</span>
     </div>
   );
 };

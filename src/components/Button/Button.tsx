950e86992e92e09efe00f782b734299f480e6f15
@FranCarn <carnevalefranco99@gmail.com>
2023-04-23 19:58:26 -0300

    fix(Button): button now accepts icon & readux toolkit added

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import styles from "./Button.module.scss";
 interface Props {
   text: string;
   clickHandler?: () => void;
+  logo?: ReactElement;
 }
-export const Button = ({ text, clickHandler }: Props) => {
+export const Button = ({ text, clickHandler, logo }: Props) => {
+  if (logo)
+    return (
+      <div>
+        <button
+          className={`${styles.btnFlex} ${styles.btn}`}
+          onClick={clickHandler}
+        >
+          {text}
+          {logo}
+        </button>
+      </div>
+    );
+
   return (
     <button className={styles.btn} onClick={clickHandler}>
       {text}
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import styles from "./Navbar.module.scss";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../Button/Button";
 import { BsChevronDown } from "react-icons/bs";
+import { IoMdCart } from "react-icons/io";
 
 export const Navbar = () => {
   const [open, setOpen] = useState<boolean>(false);
@@ -22,7 +23,7 @@ export const Navbar = () => {
       />
 
       <div className={open ? styles.dropDownOpen : styles.dropDownClosed}>
-        <Button text="Cart" />
+        <Button text="Cart" logo={<IoMdCart />} />
         <Button clickHandler={logout} text="Logout" />
       </div>
     </div>

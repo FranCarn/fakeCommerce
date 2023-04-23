769513c53cbbe82ba7cb3ab29b6bfc5784ef3345
@FranCarn <carnevalefranco99@gmail.com>
2023-04-23 19:48:39 -0300

    feat(sidebar): new dropdown to open card done

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.scss";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../Button/Button";
+import { BsChevronDown } from "react-icons/bs";
 
 export const Navbar = () => {
+  const [open, setOpen] = useState<boolean>(false);
   const navigate = useNavigate();
   const logout = () => {
     localStorage.removeItem("token");
@@ -12,7 +14,17 @@ export const Navbar = () => {
   return (
     <div className={styles.navbarContainer}>
       <span>FAKE STORE</span>
-      <Button clickHandler={logout} text="Logout" />
+
+      <BsChevronDown
+        size={20}
+        className={styles.profileDropDown}
+        onClick={() => setOpen((prev) => !prev)}
+      />
+
+      <div className={open ? styles.dropDownOpen : styles.dropDownClosed}>
+        <Button text="Cart" />
+        <Button clickHandler={logout} text="Logout" />
+      </div>
     </div>
   );
 };
